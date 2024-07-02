f222b867578a37a422abc0e723aba124f6f03ac7
WAHU0508 <sharongikenye@gmail.com>
2024-07-02 17:57:50 +0300

    Edit file

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    { year: "2012", result: "N/A"},
-    { year: "2011", result: "L"},
-    { year: "2010", result: "W"}
-  ]
-  function superbowlWin(array) {
-        const winningYear = array.find(element => element.result === 'W')
-        if (winningYear) {
-            return winningYear.year
-        }
-        else {
-            return undefined
-        }
-  }
+function superbowlWin(array) {
+    const winningYear = array.find(element => element.result === 'W')
+    if (winningYear) {
+        return winningYear.year
+    }
+    else {
+        return undefined
+    }
+}
